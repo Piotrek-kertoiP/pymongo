9bdbbe235201c44744a68c26a756562113a21e58
Piotr Szczerbiński <piotr.szczerbinski@lekta.ai>
2021-06-14 16:44:56 +0200

    main menu done

--- python/mongo_client.py
+++ python/mongo_client.py
@@ -8,18 +8,36 @@ def insert_dataset(iris_dataset_file_path, collection):
     pass
 
 
+def create_record():
+    pass
+
+
+def retrieve_record():
+    pass
+
+
+def update_record():
+    pass
+
+
+def delete_record():
+    pass
+
+
 def main():
     print('Starting...\n')
-    
-   # mongo_client = MongoClient(host = 'localhost:27017', username = dbuser, password = dbpassword)
-    mongo_client = MongoClient(host = '172.18.0.3:27017', username = dbuser, password = dbpassword)
+
+    dbHost = input('Provide mongo IP\n')
+    dbPort = 27017
+
+    mongo_client = MongoClient(host = str(dbHost) + ':' + str(dbPort), username = dbuser, password = dbpassword)
     print('Created Mongo client\n')
 
-    print("Databases: ", mongo_client.list_database_names(), '\n')
+    print("Databases: " + str(mongo_client.list_database_names()), '\n')
     chosen_db_name = input('Choose database to use:\n')
     db = mongo_client[chosen_db_name]
 
-    print('Available collections', db.list_collections(), '\n')
+    print('Available collections' + str(db.list_collections()), '\n')
     chosen_collection_name = input('Choose collection to use:\n')
     collection = db[chosen_collection_name]
 
@@ -31,6 +49,17 @@ def main():
 
         if operation == 'exit':
             should_continue = False
+        elif operation == 'C':
+            create_record()
+        elif operation == 'R':
+            retrieve_record()
+        elif operation ==  'U':
+            update_record()
+        elif operation ==  'D':
+            delete_record()
+        else:
+            print("Wrong command\n")
+
 
 main()
 
